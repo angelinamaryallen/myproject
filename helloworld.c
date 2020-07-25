de1e087eb98e2bcdf1126466ef56d7eec5582d1c
angelinamaryallen <angelinamaryallen@gmail.com>
2020-07-25 11:38:00 +0530

    first commit

---
+++ helloworld.c
@@ -0,0 +1,12 @@
+sudo apt install git-all
+git init
+git config --list --show-origin
+git config --global user.name "angelinamaryallen"
+git config --global user.email angelinamaryallen@gmail.com
+git config --list
+sudo snap install notepad-plus-plus
+Go to your github account and create a repository with a name myproject
+mkdir myproject (back in terminal)
+cd myproject
+git init
+git remote add origin  https://github.com/angelinamaryallen/myproject.git
